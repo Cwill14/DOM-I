500bf25c535712334d96336bf714a40564fcc73a
Clark Williams <theclarkwilliams@gmail.com>
2019-05-13 14:25:44 -0600

    updated img sources

--- js/index.js
+++ js/index.js
@@ -40,3 +40,11 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent.cta["img-src"]);
+
+let midImg = document.getElementById("middle-img");
+// midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+midImg.src = siteContent["main-content"]["middle-img-src"];
