0a0d3f064237b6e3b4737bc65c65a354ccff4347
Clark Williams <theclarkwilliams@gmail.com>
2019-05-13 16:06:56 -0600

    finished updating, only need to append/prepend now

--- js/index.js
+++ js/index.js
@@ -46,5 +46,43 @@ let headerImg = document.getElementById("cta-img");
 headerImg.setAttribute('src', siteContent.cta["img-src"]);
 
 let midImg = document.getElementById("middle-img");
-// midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 midImg.src = siteContent["main-content"]["middle-img-src"];
+
+let navItems = document.querySelectorAll('header nav a');
+navItems.forEach((item, index) => {
+  let tempKey =`nav-item-${index + 1}`;
+  item.textContent = siteContent.nav[tempKey];      // From Brandon, good idea
+  item.style.color = "green";
+})
+
+
+let h1Text = document.querySelector('h1');
+h1Text.textContent = siteContent.cta["h1"];
+
+let btnTxt = document.querySelector('button');
+btnTxt.textContent = siteContent.cta["button"];
+
+let h4Arr = document.querySelectorAll('.text-content h4');
+h4Arr[0].textContent = "Features";
+h4Arr[1].textContent = "About";
+h4Arr[2].textContent = "Services";
+h4Arr[3].textContent = "Product";
+h4Arr[4].textContent = "Vision";
+
+let mainP = document.querySelectorAll('.main-content p');
+mainP[0].textContent = siteContent["main-content"]["features-content"];
+mainP[1].textContent = siteContent["main-content"]["about-content"];
+mainP[2].textContent = siteContent["main-content"]["services-content"];
+mainP[3].textContent = siteContent["main-content"]["product-content"];
+mainP[4].textContent = siteContent["main-content"]["vision-content"];
+
+document.querySelector(".contact h4").textContent = siteContent.contact["contact-h4"];
+let contactP = document.querySelectorAll('.contact p');
+contactP[0].textContent = siteContent.contact["address"];
+contactP[1].textContent = siteContent.contact["phone"];
+contactP[2].textContent = siteContent.contact["email"];
+
+document.querySelector('footer p').textContent = siteContent.footer["copyright"];
+
+
+
